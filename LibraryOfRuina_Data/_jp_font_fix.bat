18de9b092f6fcfa6df4c66a3ccadbe092d8fbad6
lltcggie <lltcggie@gmail.com>
2020-05-25 04:49:03 +0900

    ステータスにカーソルを合わせるとポップする部分に書かれている文章などで□が出るのを修正 その部分は韓国語フォントを使用しているっぽかったので、韓国語フォントも日本語フォントに置き換えるようにした

--- LibraryOfRuina_Data/_jp_font_fix.bat
+++ LibraryOfRuina_Data/_jp_font_fix.bat
@@ -1,8 +1,13 @@
 @echo off
+UnityEX.exe export resources.assets -t -105
+UnityEX.exe export sharedassets0.assets -t 114
 UnityEX.exe export sharedassets1.assets -t -16
+_TextMeshPro_Editer.exe _jp_base_07LogoTypeGothic7.bin "Unity_Assets_Files\resources\resources_00005.-105" "Unity_Assets_Files\resources\resources_00005.-105"
+_TextMeshPro_Editer.exe _jp_base_ShipporiMincho-TTF-Regular.bin "Unity_Assets_Files\sharedassets0\sharedassets0_00002.114" "Unity_Assets_Files\sharedassets0\sharedassets0_00002.114"
 _TextMeshPro_Editer.exe _jp_base_07LogoTypeGothic7.bin "Unity_Assets_Files\sharedassets1\sharedassets1_00004.-16" "Unity_Assets_Files\sharedassets1\sharedassets1_00004.-16"
 _TextMeshPro_Editer.exe _jp_base_ShipporiMincho-TTF-Regular.bin "Unity_Assets_Files\sharedassets1\sharedassets1_00005.-16" "Unity_Assets_Files\sharedassets1\sharedassets1_00005.-16"
 _TextMeshPro_Editer.exe _jp_base_ShipporiMincho-TTF-Regular.bin "Unity_Assets_Files\sharedassets1\sharedassets1_00006.-16" "Unity_Assets_Files\sharedassets1\sharedassets1_00006.-16"
-UnityEX.exe import sharedassets1.assets -t -16
-UnityEX.exe import sharedassets1.assets -t tex
+UnityEX.exe import resources.assets
+UnityEX.exe import sharedassets0.assets
+UnityEX.exe import sharedassets1.assets
 pause
